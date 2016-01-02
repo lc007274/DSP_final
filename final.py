f8b82fd54a768fcbe89c0d20de29b1287a5a0ccc
tadpole655010 <tadpole655010@gmail.com>
2016-01-02 16:13:17 +0800

    Optimization: Replace constant 1024 with parameter

--- final.py
+++ final.py
@@ -4,10 +4,10 @@ import numpy as np
 import scipy
 #123123
 # Parameter Definition
-RATE  = 20000
+RATE  = 4800
 DELAY = 900
 SOUND_SPEED  = 341  # Theoretical sound speed (m/s)
-OBJECT_SPEED = -30  # Object moving speed (m/s)
+OBJECT_SPEED = 25  # Object moving speed (m/s)
 
 # freq_coeff is the coefficient of the frequency, depends on object speed
 freq_coeff = SOUND_SPEED/(SOUND_SPEED+OBJECT_SPEED)
@@ -22,7 +22,7 @@ def main():
                     output=True,
                     stream_callback=callback)
 
-    stream.start_stream()
+    stream.start_stream()   
     print("\n\nfreq_coeff = ", "%.2f" % freq_coeff)
     print(" obj speed = ", OBJECT_SPEED)
     while stream.is_active():
@@ -48,6 +48,7 @@ zero_array.extend([0]*len(hl))
 def callback(in_data, frame_count, time_info, status, hl=hl,hr=hr,d=zero_array[:],c=zero_array[:]):
 
     # Transform byte format in_data to float format out_data
+    data_length = int(len(in_data)/4)
     out_data = np.fromstring(in_data,dtype=np.short)
     out_data = out_data/(2.0**15)
 
@@ -59,29 +60,32 @@ def callback(in_data, frame_count, time_info, status, hl=hl,hr=hr,d=zero_array[:
         out_data_left.extend(out_data[i*2:i*2+1])
         out_data_right.extend(out_data[i*2+1:i*2+2])
 
+
     # Fourier Transform
-    FREQ = 0
     fft_left = scipy.fft(out_data_left)
     fft_right = scipy.fft(out_data_right)
     out_fft_left = fft_left
     out_fft_right = fft_right
 
+
     # Filtering in frequency domain
     FILT_LOW = 1
-    FILT_HIGH = 1000;
-    fft_left = np.concatenate(([0]*FILT_LOW, fft_left[FILT_LOW:FILT_HIGH], [0]*(1024-FILT_HIGH)))
-    fft_right = np.concatenate(([0]*FILT_LOW, fft_right[FILT_LOW:FILT_HIGH], [0]*(1024-FILT_HIGH)))
+    FILT_HIGH = data_length;
+    fft_left = np.concatenate(([0]*FILT_LOW, fft_left[FILT_LOW:FILT_HIGH], [0]*(data_length-FILT_HIGH)))
+    fft_right = np.concatenate(([0]*FILT_LOW, fft_right[FILT_LOW:FILT_HIGH], [0]*(data_length-FILT_HIGH)))
 
     # Shifting in frequency domain
+    FREQ = 0
     if FREQ>0:
-        fft_left = np.concatenate(([0]*FREQ, fft_left[:1024-FREQ]))
-        fft_right = np.concatenate(([0]*FREQ, fft_right[:1024-FREQ]))
+        fft_left = np.concatenate(([0]*FREQ, fft_left[:data_length-FREQ]))
+        fft_right = np.concatenate(([0]*FREQ, fft_right[:data_length-FREQ]))
     else:
-        fft_left = np.concatenate((fft_left[-FREQ:1024], [0]*(-FREQ)))
-        fft_right = np.concatenate((fft_right[-FREQ:1024], [0]*(-FREQ)))
+        fft_left = np.concatenate((fft_left[-FREQ:data_length], [0]*(-FREQ)))
+        fft_right = np.concatenate((fft_right[-FREQ:data_length], [0]*(-FREQ)))
+
 
     # Doopler Effect
-    for i in range(1024):
+    for i in range(data_length):
 
         # Get the index of original spectrum
         index = np.ceil(i/freq_coeff)
@@ -91,12 +95,14 @@ def callback(in_data, frame_count, time_info, status, hl=hl,hr=hr,d=zero_array[:
         high_coeff = 1 - low_coeff
 
         # Assign to scaled spectrum from linear combination of original spectrum
-        if index>1024:
-            out_fft_left[i]=0
-            out_fft_right[i]=0
+        if index>data_length-1:
+            out_fft_left[i]  = 0.6*out_fft_left[i-1]
+            out_fft_right[i] = 0.6*out_fft_right[i-1]
         else:
-            out_fft_left[i] = (low_coeff * fft_left[index-1]) + (high_coeff * fft_left[i].real)
-            out_fft_right[i] = (low_coeff * fft_right[index-1]) + (high_coeff * fft_right[i])
+            out_fft_left[i] = (low_coeff * fft_left[index-1]) + (high_coeff * fft_left[index])
+            out_fft_right[i] = (low_coeff * fft_right[index-1]) + (high_coeff * fft_right[index])
+#            print( "%.3f" % out_fft_left[i], " = ", "%.1f" % low_coeff, "*", "%.3f" % fft_left[index-1], " / ", "%.1f" % high_coeff, "*", "%.3f" % fft_left[index])
+
 
     # Inverse Fourier Transform
     out_data_left = scipy.ifft(out_fft_left)
@@ -107,6 +113,7 @@ def callback(in_data, frame_count, time_info, status, hl=hl,hr=hr,d=zero_array[:
     m = len(hl)
     l = len(out_data_left)
 
+#    print("checkpoint2")
     # put left & right together
     out_data2 = out_data[0:n]
     for i in range(int(len(out_data)/2)):
@@ -118,6 +125,7 @@ def callback(in_data, frame_count, time_info, status, hl=hl,hr=hr,d=zero_array[:
     out = np.int16(out_data2)
     out = out.tostring()
     
+
     return (out, pyaudio.paContinue)
 
 main()
